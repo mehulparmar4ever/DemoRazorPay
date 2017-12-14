17e0a89e14ac6adaabd2b60a94dda731208ed475
Mehul <mehul.parmar@sooryen.com>
2017-12-14 18:21:36 +0530

    Initial Commit

---
+++ DemoRazorPay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoRazorPay
+//
+//  Created by Sooryen on 12/14/17.
+//  Copyright © 2017 Sooryen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
