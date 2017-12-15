3cdb3949f6447844fcd976e22bb36da1658a7d1f
Mehul <mehul.parmar@sooryen.com>
2017-12-15 12:42:44 +0530

    Demo Razorpay Payment Gateway - Easily Accept Online Payments‎
    Razorpay is a payments company that provides payment solutions to online merchants in India
    
    Accept Cards, Netbanking, Wallets & UPI. Developer Friendly API, Fast Onboarding
    No Setup Fees · Accept Netbanking, Cards
    Services: Flash Checkout, UPI Payments, eCOD

--- DemoRazorPay/ViewController.swift
+++ DemoRazorPay/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import Razorpay
+
+let KEY_ID = "rzp_test_hcN5TrrqAtN5gJ"; // @"rzp_test_1DP5mmOlF5G5ag";
+let SUCCESS_TITLE = "Yay!";
+let SUCCESS_MESSAGE = "Your payment was successful.";
+let FAILURE_TITLE = "Uh-Oh!";
+let FAILURE_MESSAGE = "Your payment failed due to an error.";
+let EXTERNAL_METHOD_TITLE = "Umm?";
+let EXTERNAL_METHOD_MESSAGE = "You selected %@, which is not supported by Razorpay at the moment.\nDo you want to handle it separately?";
+let OK_BUTTON_TITLE = "OK";
+
+extension ViewController : ExternalWalletSelectionProtocol {
+    func onExternalWalletSelected(_ walletName: String, WithPaymentData paymentData: [AnyHashable : Any]?) {
+        self.showAlert(EXTERNAL_METHOD_TITLE, message: "\(EXTERNAL_METHOD_MESSAGE) walletName :\(walletName), paymentData:\(String(describing: paymentData))")
+    }
+}
+
+extension ViewController : RazorpayPaymentCompletionProtocol {
+    func onPaymentError(_ code: Int32, description str: String) {
+        self.showAlert(FAILURE_TITLE, message: "\(FAILURE_MESSAGE) code :\(code), description str:\(str)")
+    }
+    
+    func onPaymentSuccess(_ payment_id: String) {
+        self.showAlert(SUCCESS_TITLE, message: "\(SUCCESS_MESSAGE) payment_id :\(payment_id)")
+    }
+}
 
 class ViewController: UIViewController {
-
+    var razorpay : Razorpay?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        razorpay = Razorpay.initWithKey(KEY_ID, andDelegate: self)
+        razorpay?.setExternalWalletSelectionDelegate(self)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func btnPayClicked(_ sender: Any) {
+        let options : Dictionary<String, Any> = ["amount":"100", "currency":"INR", "description":"Fine T-shirt", "image":#imageLiteral(resourceName: "logo"), "name":"Razorpay", "external":["wallets":["paytm"]], "prefill":["email":"contact@razorpay.com", "contact":"18002700323"], "theme":["color":"#3594E2"]]
+        razorpay?.open(options)
+    }
+    
+    func showAlert(_ title:String, message:String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        UIApplication.shared.beginIgnoringInteractionEvents()
 
-
+        self.present(alertController, animated: true, completion: {
+            if UIApplication.shared.isIgnoringInteractionEvents {
+                UIApplication.shared.endIgnoringInteractionEvents()
+            }
+        })
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, ‘10.0’
+use_frameworks!
+inhibit_all_warnings!
+
+target 'DemoRazorPay' do
+	pod 'razorpay-pod', '1.0.6'
+end
